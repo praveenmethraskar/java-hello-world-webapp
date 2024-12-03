4333cf9729c0725a01731d016627807668a62682
Praveen Methraskar <55995179+praveenmethraskar@users.noreply.github.com>
2024-12-03 16:01:14 +0530

    Update grov.groovy

--- grov.groovy
+++ grov.groovy
@@ -1,6 +1,8 @@
 pipeline{
     agent any
-    
+    environment{
+        PATH = "/opt/maven3/bin:$PATH"
+    }
     stages{
         stage('clone'){
             steps{
@@ -9,14 +11,23 @@ pipeline{
         }
         stage('build maven'){
             steps{
-                sh ''' mvn clean install '''
+                sh "mvn clean deploy"
+                sh "mv target/*.war target/myweb.war"
             }
         }
   
         
         stage('deploy'){
             steps{
-                deploy adapters: [tomcat9(credentialsId: 'admin', path: '', url: 'http://3.110.119.107:8080/')], contextPath: 'myapp1', war: '**/*.war'
+                sshagent(['DEPLOYMENT']) {
+                sh """
+            scp -o StrictHostKeyChecking=no target/myweb.war  root@3.110.119.107:/home/ubuntu/tomcat/webapps/
+                    
+                    ssh ec2-user@3.110.119.107 /home/ec2-user/tomcat9/bin/shutdown.sh
+                    
+                    ssh ec2-user@3.110.119.107 /home/ec2-user/tomcat9/bin/startup.sh
+                     """
+                }
             }
         }
     }
